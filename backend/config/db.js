f9cee31ea2e1152c5830a8efc284cf165a11bb5c
Anderpott33 <anderpott33@gmail.com>
2026-04-05 19:42:38 -0300

    Cambio de iPV6 a IPV4 para supabase

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,14 @@
-
-import pkg from 'pg'
+import pkg from 'pg';
 import dotenv from 'dotenv';
 dotenv.config();
-const { Pool } = pkg
+const { Pool } = pkg;
 
+// Forzar conexión IPv4
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false
-  }
-})
+  },
+  port: 5432,   // puerto de Postgres
+  family: 4     // 👈 fuerza IPv4
+});
