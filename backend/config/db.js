c62822c9beff8fea4ccc487cb8470148af01bf32
Anderpott33 <anderpott33@gmail.com>
2026-04-05 19:55:05 -0300

    Probando subir el back a render

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,12 +3,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 const { Pool } = pkg;
 
-// Forzar conexión IPv4
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  },
-  port: 5432,   // puerto de Postgres
-  family: 4     // 👈 fuerza IPv4
+  ssl: { rejectUnauthorized: false },
+  family: 4
 });
